705276020ba0c60bd08544628c085f53a65f2c04
bean8080 <ahyeon041970@gmail.com>
2026-04-26 14:19:40 +0900

    init: Spring Boot + Docker(PostgreSQL, Redis) setup with hello API

---
+++ src/main/java/com/ahyeon/flowbit/FlowbitApplication.java
@@ -0,0 +1,13 @@
+package com.ahyeon.flowbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowbitApplication.class, args);
+    }
+
+}
