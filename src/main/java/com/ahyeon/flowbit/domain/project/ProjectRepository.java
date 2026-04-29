22b221559824883febeeb76fbf4b840dfb7aa9a7
bean8080 <ahyeon041970@gmail.com>
2026-04-29 23:13:39 +0900

    feat: add project repository

---
+++ src/main/java/com/ahyeon/flowbit/domain/project/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.ahyeon.flowbit.domain.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findByStatus(ProjectStatus status);
+
+    List<Project> findByStatusNot(ProjectStatus status);
+}
