3cc11ca51a842a625392afdf4383f1787e66a7cb
bean8080 <ahyeon041970@gmail.com>
2026-05-21 23:08:00 +0900

    refactor: improve global exception handling

--- src/main/java/com/ahyeon/flowbit/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ahyeon/flowbit/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.ahyeon.flowbit.common.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,9 +24,40 @@ public class GlobalExceptionHandler {
         return new ErrorResponse("CONFLICT", e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    public ErrorResponse handleBadCredentialsException(BadCredentialsException e) {
+        return new ErrorResponse("UNAUTHORIZED", "이메일 또는 비밀번호가 올바르지 않습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ErrorResponse("CONFLICT", "이미 사용 중인 이메일입니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(error -> error.getDefaultMessage())
+                .orElse("잘못된 요청입니다.");
+
+        return new ErrorResponse("BAD_REQUEST", message);
+    }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ErrorResponse handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
         return new ErrorResponse("BAD_REQUEST", "요청 파라미터 값이 올바르지 않습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorResponse handleException(Exception e) {
+        return new ErrorResponse("INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.");
     }
 }
